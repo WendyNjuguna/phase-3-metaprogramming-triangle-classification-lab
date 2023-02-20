8e1fd0aec58193a5078b010129862bd4096c27cd
WendyNjuguna <wendynjuguna714@gmail.com>
2023-02-20 10:38:17 +0300

    commit tree 226015fd38bfbc4dd1ee8fa24a962e6106bbceaa

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles   = isosceles
+    @scalene     = scalene
+  end
+  # kind
+  # if else: sideA==sideB && sideB == sideC return :equilateral
+  # elsif: sideA == sideB || sideA == sideC || sideA || sideB == sideC return :isosceles
+  # else return :scalene
+  def kind
+  validate_triangle
+  if (equilateral == isosceles && isosceles == scalene)
+    :equilateral
+  elsif (equilateral == isosceles || isosceles == scalene || equilateral == scalene)
+    :isosceles
+  else
+    :scalene
+  end
+
+end
+
+  def validate_triangle
+    sides = [equilateral, isosceles, scalene].sort
+    raise TriangleError unless sides.all?(&:positive?)
+    raise TriangleError unless sides[0] + sides[1] > sides[2]
+    end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
